64f66cd0c97ef9ef387da6e79584ca0b48d8cee4
Fauzan Ali <fauzanalivijsma@gmail.com>
2025-05-01 15:38:23 +0700

    fix

--- src/be-validify/src/index.ts
+++
@@ -1,6 +0,0 @@
-// routes/userRoute.ts
-import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from './controllers/userController';
-
-export const UserRoutes = {
-    createUser, getAllUsers, getUserById, updateUser, deleteUser
-};
