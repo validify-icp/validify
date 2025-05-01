b43948e521af41dd4eba71ee0990e51d785ab716
Fauzan Ali <fauzanalivijsma@gmail.com>
2025-05-01 15:24:36 +0700

    fix: code for deploy

--- src/motoko-try-frontend/src/main.tsx
+++ src/motoko-try-frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import { Provider } from "react-redux";
 
-import App from "./App.tsx";
-import store from "./store/index.ts";
+import App from "./App.js";
+import store from "./store/index.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
