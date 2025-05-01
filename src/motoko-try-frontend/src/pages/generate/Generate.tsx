8e77ac17c82fcf27a04363550a1503cf210a1af8
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-01 18:58:46 +0700

    feat: adding modal in generate page.

--- src/motoko-try-frontend/src/pages/generate/Generate.tsx
+++ src/motoko-try-frontend/src/pages/generate/Generate.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
-import Navbar from "../../components/Navbar";
-import Button from "../../components/Button";
+import Navbar from "../../components/molecules/Navbar";
+import Button from "../../components/elements/Button";
 
 import { backgrounds, images } from "../../constants";
+import ModalNewCertificate from "../../components/molecules/ModalNewCertificate";
 
 const GeneratePage = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const navigate = useNavigate();
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
   const handleNavigate = (href: string) => {
     navigate(href);
@@ -14,6 +23,7 @@ const GeneratePage = () => {
 
   return (
     <div className="w-full h-[1200px] font-dm-sans">
+      <ModalNewCertificate open={showModal} onClose={toggleModal} />
       <Navbar logo_color="text-[#653FFF]" menu_color="text-black" />
       <div
         className="h-full px-20 pt-3 bg-cover bg-no-repeat bg-[position:10%_10%]"
@@ -47,7 +57,10 @@ const GeneratePage = () => {
             <div className="w-full h-full mx-auto">
               <div className="w-full flex items-center justify-between">
                 <h3 className="text-3xl font-medium">Generate</h3>
-                <Button label="Generate New Certificate" />
+                <Button
+                  label="Generate New Certificate"
+                  onClick={toggleModal}
+                />
               </div>
               <div className="mt-8 w-full flex items-center justify-between">
                 <input
@@ -57,8 +70,11 @@ const GeneratePage = () => {
                 />
               </div>
               <div className="mt-8 w-full flex flex-wrap gap-5 items-center justify-between">
-                {Array.from({ length: 8 }).map((_, i) => (
-                  <div className="px-4 py-6 flex flex-col justify-between gap-5 w-[280px] h-[330px] rounded-md border-2 border-slate-200/40">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <div
+                    className="px-4 py-6 flex flex-col justify-between gap-5 w-[280px] h-[330px] rounded-md border-2 border-slate-200/40"
+                    key={i}
+                  >
                     <div className="flex flex-col gap-5">
                       <div>
                         <span className="text-[#43936C] border border-[#b8dbca] px-2 py-1 rounded-md bg-[#F6F6F6]">
@@ -76,6 +92,32 @@ const GeneratePage = () => {
                     </div>
                     <Button
                       label="See Result"
+                      onClick={() => handleNavigate("/generate/1/result")}
+                    />
+                  </div>
+                ))}
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <div
+                    className="px-4 py-6 flex flex-col justify-between gap-5 w-[280px] h-[330px] rounded-md border-2 border-slate-200/40"
+                    key={i}
+                  >
+                    <div className="flex flex-col gap-5">
+                      <div>
+                        <span className="text-[#43936C] border border-[#b8dbca] px-2 py-1 rounded-md bg-[#F6F6F6]">
+                          Published
+                        </span>
+                      </div>
+                      <h4 className="w-[80%] text-xl font-medium">
+                        Sertifikat Kampus Merdeka Batch 2 - 2022
+                      </h4>
+                      <div>
+                        <span className=" text-[#4E5BA6] px-2 py-1 rounded-md bg-[#F2F4F9]">
+                          Webinar
+                        </span>
+                      </div>
+                    </div>
+                    <Button
+                      label="Edit"
                       onClick={() => handleNavigate("/generate/1")}
                     />
                   </div>
