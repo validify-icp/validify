2044d11ed47d047471d2d16ab8f17ac863574f51
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-01 14:02:33 +0700

    feat: update navbar colors when its scrolls.

--- src/fe-validify/src/components/Navbar.tsx
+++ src/fe-validify/src/components/Navbar.tsx
@@ -1,23 +1,80 @@
-import logo from "../assets/images/logo.png";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import Button from "./Button";
 
-const Navbar = () => {
+import { logos } from "../constants";
+
+const Navbar = ({ logo_color = "text-white", menu_color = "text-white" }) => {
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 10) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed z-[999] top-4 left-10 right-10 flex items-center justify-between border px-5 py-2 border-slate-200 rounded-lg bg-white/20 backdrop-blur-3xl">
-      <div className="flex items-center gap-2">
-        <img src={logo} width={30} alt="logo" />
-        <span className="font-bold text-2xl text-white">Validify</span>
-      </div>
+    <header className="fixed z-[999] top-4 left-20 right-20 flex items-center justify-between border px-5 py-2 border-slate-200 rounded-lg bg-white/20 backdrop-blur-3xl">
+      <NavLink to="/" className="flex items-center gap-2">
+        {logo_color === "text-white" && (
+          <img
+            src={isScroll ? logos.main_purple : logos.main}
+            width={30}
+            alt="logo"
+          />
+        )}
+        {logo_color !== "text-white" && (
+          <img src={logos.main_purple} width={30} alt="logo" />
+        )}
+        <span
+          className={`font-bold text-2xl ${
+            isScroll ? "text-[#653FFF]" : logo_color
+          }`}
+        >
+          Validify
+        </span>
+      </NavLink>
       {true && (
         <nav>
-          <ul>
-            <li className="flex gap-8 text-white">
-              <a href="#" className="font-bold">
+          <ul
+            className={`flex gap-8 text-base ${
+              isScroll ? "text-black" : menu_color
+            }`}
+          >
+            <li className="group">
+              <NavLink
+                to="/verify"
+                className={({ isActive }) => (isActive ? "font-bold" : "")}
+              >
                 Verify
-              </a>
-              <a href="#">Generate</a>
-              <a href="#">Validate</a>
+              </NavLink>
+            </li>
+            <li className="group">
+              <NavLink
+                to="/generate"
+                className={({ isActive }) => (isActive ? "font-bold" : "")}
+              >
+                Generate
+              </NavLink>
+            </li>
+            <li className="group">
+              <NavLink
+                to="/validate"
+                className={({ isActive }) => (isActive ? "font-bold" : "")}
+              >
+                Validate
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -27,7 +84,11 @@ const Navbar = () => {
           <span className="bg-[#EFF3FA] px-2 py-2 text-black rounded-2xl">
             NA
           </span>
-          <span className="text-white font-medium">Nadia Omara</span>
+          <span
+            className={`font-medium ${isScroll ? "text-black" : menu_color}`}
+          >
+            Nadia Omara
+          </span>
         </div>
       )}
       {false && (
