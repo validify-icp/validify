c7e804ac07009883ec4050fb9824c36198047bf5
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-01 14:01:17 +0700

    feat: adding route for generate features.

--- src/fe-validify/src/router.tsx
+++ src/fe-validify/src/router.tsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home/Home";
+import GeneratePage from "./pages/generate/Generate";
+import DetailGeneratePage from "./pages/generate/Detail";
+import ResultGeneratePage from "./pages/generate/Result";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     id: "root",
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "generate",
+        children: [
+          { index: true, element: <GeneratePage /> },
+          {
+            path: ":id",
+            element: <DetailGeneratePage />,
+          },
+          {
+            path: ":id/result",
+            element: <ResultGeneratePage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
