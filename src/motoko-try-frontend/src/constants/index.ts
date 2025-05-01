15a24ac8469ee6f4945f24d038b1e9f20ceb1f88
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-01 18:54:31 +0700

    feat: adding icons and images update constant.

--- src/motoko-try-frontend/src/constants/index.ts
+++ src/motoko-try-frontend/src/constants/index.ts
@@ -6,6 +6,7 @@ import background from "../assets/images/bg-generate.png";
 import hero from "../assets/images/bg-hero.png";
 import template_sertif_1 from "../assets/images/template-sertif-1.png";
 import generate_not_found from "../assets/images/generate-not-found.png";
+import template_guideline from "../assets/images/template-guideline.png";
 
 import arrow_left from "../assets/icons/arrow_left.svg";
 import arrow_left_white from "../assets/icons/arrow_left_white.svg";
@@ -15,11 +16,17 @@ import download_2 from "../assets/icons/download_2.svg";
 import edit from "../assets/icons/edit.svg";
 import question_mark from "../assets/icons/question_mark.svg";
 import upload from "../assets/icons/upload.svg";
+import close from "../assets/icons/close.svg";
+import size_guideline from "../assets/icons/size_guideline.svg";
+import size_guideline_purple from "../assets/icons/size_guideline_purple.svg";
+import drop from "../assets/icons/drop.svg";
+import action from "../assets/icons/action.svg";
 
 export const images = {
   hero,
   template_sertif_1,
   generate_not_found,
+  template_guideline,
 };
 
 export const logos = {
@@ -40,4 +47,9 @@ export const icons = {
   edit,
   question_mark,
   upload,
+  close,
+  size_guideline,
+  size_guideline_purple,
+  drop,
+  action,
 };
