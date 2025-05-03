ee1c729d585d3294bc64c3e0d7e4592677083b86
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-04 01:43:57 +0700

    stye: update styling formatting in several components

--- src/motoko-try-frontend/src/components/elements/Button.tsx
+++ src/motoko-try-frontend/src/components/elements/Button.tsx
@@ -5,14 +5,32 @@ interface ButtonProps {
   children?: ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-const Button = ({ label, children, className = "", onClick }: ButtonProps) => {
+const Button = ({
+  label,
+  children,
+  className = "",
+  onClick,
+  disabled = false,
+}: ButtonProps) => {
+  const enabledClasses =
+    "bg-gradient-to-t from-[#6240ED] to-[#917AF2] hover:bg-gradient-to-b hover:from-[#6240ED] hover:to-[#917AF2]";
+  const disabledClasses =
+    "!bg-slate-200 !text-gray-400 !border-slate-200 !outline-none cursor-not-allowed";
+
   const baseClasses =
-    "px-4 py-2 text-white font-medium rounded-lg border border-[#a693f5] outline outline-white bg-gradient-to-t from-[#6240ED] to-[#917AF2] shadow-md cursor-pointer hover:bg-gradient-to-b hover:from-[#6240ED] hover:to-[#917AF2]";
+    "px-4 py-2 text-white font-medium rounded-lg border border-[#a693f5] outline outline-white shadow-md cursor-pointer";
 
   return (
-    <button onClick={onClick} className={`${baseClasses} ${className}`}>
+    <button
+      onClick={onClick}
+      className={`${baseClasses} ${
+        disabled ? disabledClasses : enabledClasses
+      } ${className}`}
+      disabled={disabled}
+    >
       {children || label || "Button"}
     </button>
   );
