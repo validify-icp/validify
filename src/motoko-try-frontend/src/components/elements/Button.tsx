b7297dcc4f66f2062a49dad123f7fc2dbee27429
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-04 13:49:28 +0700

    style: update styling button when hover.

--- src/motoko-try-frontend/src/components/elements/Button.tsx
+++ src/motoko-try-frontend/src/components/elements/Button.tsx
@@ -16,19 +16,19 @@ const Button = ({
   disabled = false,
 }: ButtonProps) => {
   const enabledClasses =
-    "bg-gradient-to-t from-[#6240ED] to-[#917AF2] hover:bg-gradient-to-b hover:from-[#6240ED] hover:to-[#917AF2]";
+    "bg-gradient-to-t from-[#6240ED] to-[#917AF2] hover:bg-gradient-to-b hover:from-[#6240ED] hover:to-[#917AF2] cursor-pointer";
   const disabledClasses =
     "!bg-slate-200 !text-gray-400 !border-slate-200 !outline-none cursor-not-allowed";
 
   const baseClasses =
-    "px-4 py-2 text-white font-medium rounded-lg border border-[#a693f5] outline outline-white shadow-md cursor-pointer";
+    "px-4 py-2 text-white font-medium rounded-lg border border-[#a693f5] outline outline-white shadow-md";
 
   return (
     <button
       onClick={onClick}
       className={`${baseClasses} ${
         disabled ? disabledClasses : enabledClasses
-      } ${className}`}
+      } ${className} cursorno`}
       disabled={disabled}
     >
       {children || label || "Button"}
