b96704cbe7bf701d1e149bea248c85b8714830f2
Daniel Alvaro <danielkisaran@gmail.com>
2025-05-05 16:27:46 +0700

    feat: adding responsive design in navbar and homepage.

--- src/motoko-try-frontend/src/pages/home/Home.tsx
+++ src/motoko-try-frontend/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import { useNavigate } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 
 import Button from "../../components/elements/Button";
 import Input from "../../components/elements/Input";
@@ -44,22 +45,36 @@ const HomePage = () => {
       >
         <Navbar />
         {loading && <Loading />}
-        <div className="mt-20 py-10">
-          <div className="w-[60%] border-yellow-500 mx-auto">
-            <h1 className="text-5xl text-white text-center font-bold">
-              Build Trust with Every Certificate, <br /> Ready in Minutes!
+        <div className="mt-20 py-10 max-sm:mt-0">
+          <div className="w-[60%] mx-auto max-sm:w-full">
+            <h1 className="text-5xl text-white text-center font-bold whitespace-pre-line max-sm:text-3xl">
+              <span>
+                <TypeAnimation
+                  sequence={[
+                    "Build Trust with Every Certificate,\nReady in Minutes!",
+                    2000,
+                    "",
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={40}
+                  repeat={Infinity}
+                />
+              </span>
             </h1>
           </div>
-          <div className="w-[65%] h-[420px] mt-8 px-5 py-5 border border-white/25 bg-white/25 backdrop-blur-sm mx-auto rounded-lg shadow-md">
-            <div className="h-full flex flex-col gap-5 items-center justify-center border border-slate-200/20 bg-white rounded-lg px-3 py-3">
-              <h2 className="text-3xl font-bold">Drag, Drop, and Done</h2>
-              <p className="mt-2 text-xl text-center">
+          <div className="w-[65%] h-[420px] mt-8 px-5 py-5 border border-white/25 bg-white/25 backdrop-blur-sm mx-auto rounded-lg shadow-md max-sm:w-[90%] max-sm:h-[300px]">
+            <div className="h-full flex flex-col gap-5 items-center justify-center border border-slate-200/20 bg-white rounded-lg px-3 py-3 max-sm:p-4">
+              <h2 className="text-3xl font-bold max-sm:text-xl">
+                Drag, Drop, and Done
+              </h2>
+              <p className="mt-2 text-xl text-center max-sm:text-sm">
                 Drop it like it’s hot 🔥 <br /> Validify tells you if that cert
                 is real or cap — in seconds.
               </p>
               <label
                 htmlFor="fileInput"
-                className="mt-5 px-7 py-2 text-white text-lg font-normal rounded-lg border border-[#a693f5] outline outline-slate-200 bg-linear-to-t from-[#6240ED] to-[#917AF2] shadow-md cursor-pointer hover:bg-linear-to-b hover:from-[#6240ED] hover:to-[#917AF2]"
+                className="mt-5 px-7 py-2 text-white text-lg font-normal rounded-lg border border-[#a693f5] outline outline-slate-200 bg-linear-to-t from-[#6240ED] to-[#917AF2] shadow-md cursor-pointer hover:bg-linear-to-b hover:from-[#6240ED] hover:to-[#917AF2] max-sm:text-sm max-sm:mt-0"
               >
                 Select Certificate File
               </label>
@@ -71,7 +86,9 @@ const HomePage = () => {
                 onChange={handleFiles}
                 hidden
               />
-              <p className="mt-[-10px] text-xl">or drop file here</p>
+              <p className="mt-[-10px] text-xl max-sm:text-sm">
+                or drop file here
+              </p>
             </div>
           </div>
         </div>
@@ -113,15 +130,15 @@ const HomePage = () => {
           </p>
         </section>
 
-        <section className="mt-32 w-[80%] flex gap-5 items-start justify-center mx-auto">
-          <div className="w-1/3 flex flex-col items-center justify-center gap-2">
+        <section className="mt-32 w-[80%] flex gap-5 items-start justify-center mx-auto max-sm:flex-col max-sm:items-center">
+          <div className="w-1/3 flex flex-col items-center justify-center gap-2 max-sm:w-full">
             <img src={icons.verified} width={50} alt="logo purple" />
             <h3 className="text-2xl font-bold">Certificate Verification</h3>
             <p className="text-xl text-[#676767] text-center">
               Quickly check the authenticity of certificates through our system
             </p>
           </div>
-          <div className="w-1/3 flex flex-col items-center justify-center gap-2">
+          <div className="w-1/3 flex flex-col items-center justify-center gap-2 max-sm:w-full">
             <img src={icons.certified} width={50} alt="logo purple" />
             <h3 className="text-2xl font-bold">Certificate Registration</h3>
             <p className="text-xl text-[#676767] text-center">
@@ -129,7 +146,7 @@ const HomePage = () => {
               legitimacy
             </p>
           </div>
-          <div className="w-1/3 flex flex-col items-center justify-center gap-2">
+          <div className="w-1/3 flex flex-col items-center justify-center gap-2 max-sm:w-full">
             <img src={icons.monitoring} width={50} alt="logo purple" />
             <h3 className="text-2xl font-bold">Monitoring Dashboard</h3>
             <p className="text-xl text-[#676767] text-center">
@@ -138,10 +155,10 @@ const HomePage = () => {
           </div>
         </section>
 
-        <section className="mt-32 w-full flex gap-5 items-start justify-start mx-auto mb-10">
+        <section className="mt-32 w-full flex gap-5 items-start justify-start mx-auto mb-10 max-sm:flex-col max-sm:items-center">
           {Array.from({ length: 4 }, (_, index) => (
             <div
-              className="w-1/4 px-3 py-5 flex flex-col items-center justify-center gap-1 bg-[#3E66DF] rounded-xl"
+              className="w-1/4 px-3 py-5 flex flex-col items-center justify-center gap-1 bg-[#3E66DF] rounded-xl max-sm:w-[90%]"
               key={index}
             >
               <img
@@ -178,15 +195,15 @@ const HomePage = () => {
             Join thousands of users who already trust Validify!
           </p>
           <div className="flex items-center gap-5">
-            <Button label="Sign Up Now!" />
+            <Button label="Sign Up Now!" className="max-sm:text-sm" />
             <p className="font-bold text-xl">Or</p>
             <p className="font-bold text-xl text-[#0C6FD9]">Try it for free</p>
           </div>
         </section>
 
-        <footer className="mt-32 px-32 py-14 w-full mx-auto mb-10 bg-gradient-to-r from-[#653FFF] to-[#1C2792]">
-          <div className="w-full flex justify-around items-start gap-2">
-            <div className="w-1/4 flex flex-col items-center justify-center gap-8">
+        <footer className="mt-32 px-32 py-14 w-full mx-auto mb-10 bg-gradient-to-r from-[#653FFF] to-[#1C2792] max-sm:px-0">
+          <div className="w-full flex justify-around items-start gap-2 max-sm:flex-col max-sm:items-center">
+            <div className="w-1/4 flex flex-col items-center justify-center gap-8 max-sm:w-full max-sm:gap-2">
               <div className="flex items-center gap-2 text-white">
                 <img src={logos.main} width={30} alt="logo purple" />
                 <span className="font-bold text-2xl">Validify</span>
@@ -196,29 +213,32 @@ const HomePage = () => {
                 <p className="text-lg text-white">support@Validify</p>
               </div>
             </div>
-            <div className="w-[35%] flex flex-col justify-center gap-8">
+            <div className="w-[35%] flex flex-col justify-center gap-8 max-sm:w-full max-sm:items-center max-sm:gap-2">
               <p className="text-xl text-white font-bold">Quick Links</p>
-              <div className="w-full flex items-center justify-between">
-                <div className="flex flex-col items-start gap-8">
+              <div className="w-full flex items-center justify-between max-sm:flex-col max-sm:gap-2">
+                <div className="flex flex-col items-start gap-8 max-sm:gap-2 max-sm:items-center">
                   <p className="text-xl text-white/50 text-center">Sign In</p>
                   <p className="text-xl text-white/50 text-center">
                     Information
                   </p>
                 </div>
-                <div className="flex flex-col items-start gap-8">
+                <div className="flex flex-col items-start gap-8 max-sm:gap-2 max-sm:items-center">
                   <p className="text-xl text-white/50 text-center">Subscribe</p>
                   <p className="text-xl text-white/50 text-center">Validify</p>
                 </div>
               </div>
             </div>
-            <div className="w-1/4 flex flex-col items-start justify-center gap-5">
+            <div className="w-1/4 flex flex-col items-start justify-center gap-5 max-sm:w-full max-sm:items-center">
               <h3 className="text-xl font-bold text-white">Subscribe</h3>
-              <Input placeholder="Get product updates" className="bg-white" />
+              <Input
+                placeholder="Get product updates"
+                className="bg-white max-sm:w-1/2"
+              />
             </div>
           </div>
           <hr className="mt-16 mb-5 border-none bg-[#D8DCDF]/50 h-[2px]" />
-          <div className="mt-10 w-full flex justify-around items-center gap-2">
-            <div className="w-[28%] flex items-center gap-2">
+          <div className="mt-10 w-full flex justify-around items-center gap-2 max-sm:flex-col">
+            <div className="w-[28%] flex items-center gap-2 max-sm:text-xl">
               <span className="w-10 h-10 p-2 flex items-center justify-center border-2 border-white/50 rounded-full">
                 <img src={icons.linkedin} alt="linkedin icon" />
               </span>
@@ -229,7 +249,7 @@ const HomePage = () => {
                 <img src={icons.twitter} alt="twitter icon" />
               </span>
             </div>
-            <div className="w-[28%] flex justify-center items-center gap-2">
+            <div className="w-[28%] flex justify-center items-center gap-2 max-sm:w-full">
               <div className="flex justify-around items-center gap-5">
                 <p className="text-white text-lg">A product of </p>
                 <div className="flex items-center gap-2 text-white">
@@ -238,7 +258,7 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-            <div className="w-[28%] flex justify-end items-center gap-2">
+            <div className="w-[28%] flex justify-end items-center gap-2 max-sm:w-full max-sm:justify-center">
               <p className="text-white text-lg">
                 © 2025 Validify. All rights reserved
               </p>
